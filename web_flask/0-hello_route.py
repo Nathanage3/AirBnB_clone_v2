f1e5945288168162d17770f5fde511cdc8700e33
Nathanage3 <sisay954@gmail.com>
2024-02-01 10:49:11 +0300

    task #0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""This script starts a Flask web application."""
-
+"""
+flask model for route
+"""
 from flask import Flask
 
-app = Flask('__name__')
+
+app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-  """Display 'Hello HBNB!' when the root URL is accessed."""
-  return "Hello HBNB!"
+def hbnb():
+    """
+        hbnb route page
+    """
+    return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
